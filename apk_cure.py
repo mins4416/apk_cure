ffb59f1a497a07fadde26d5b590b1147bd5b9a8a
hj <hyzzish@gmail.com>
2024-03-22 14:21:33 +0900

    Check NameSpaceChunk in AndroidManifest.xml

--- apk_cure.py
+++ apk_cure.py
@@ -25,6 +25,27 @@ int_scStringCount = int.from_bytes(bytes_content[0x10:0x10+0x4], 'little')
 int_scStringPoolOffset = int.from_bytes(bytes_content[0x1c:0x1c+4], 'little')
 real_scStringCount = int((int_scStringPoolOffset+0x8-0x24)/4)
 
+
+# parse header
+int_headerSize = int.from_bytes(bytes_content[0x2:0x4], 'little')
+int_fileSize = int.from_bytes(bytes_content[0x4:0x8], 'little')
+
+# parse string chunk
+offset_sc = int_headerSize
+int_scSize = int.from_bytes(bytes_content[offset_sc+0x4:offset_sc+0x4+0x4], 'little')
+
+# parse resource chunk
+offset_rc = int_headerSize + int_scSize
+int_rcSize = int.from_bytes(bytes_content[offset_rc+0x4:offset_rc+0x4+0x4], 'little')
+
+# parse namespace chunk
+offset_nsc_start = int_headerSize + int_scSize + int_rcSize
+int_nameSpaceChunk_start = int.from_bytes(bytes_content[offset_nsc_start+0x4:offset_nsc_start+0x4+0x14], 'little')
+
+offset_nsc_end = int_fileSize - 0x18
+int_nameSpaceChunk_end = int.from_bytes(bytes_content[offset_nsc_end+0x4:offset_nsc_end+0x4+0x14], 'little')
+
+
 # You can modify data now.
 barray_content = bytearray(bytes_content)
 
@@ -45,6 +66,29 @@ if int_scStringCount is not real_scStringCount:
 else:
     print("Source's scSringCount looks good.")
 
+print("Checking... chunk")
+
+# check namespace type
+real_nameSpace_start_type = b'\x00\x01\x10\x00'
+real_nameSpace_end_type   = b'\x01\x01\x10\x00'
+byte_nameSpace_start_type = bytes_content[offset_nsc_start:offset_nsc_start+0x4]
+byte_nameSpace_end_type = bytes_content[offset_nsc_end:offset_nsc_end+0x4]
+if byte_nameSpace_start_type != real_nameSpace_start_type or byte_nameSpace_end_type != real_nameSpace_end_type:
+    print("Source's nameSpaceChunk type has a problem. Fixing...")
+    barray_content[offset_nsc_start:offset_nsc_start+0x2] = real_nameSpace_start_type
+    barray_content[offset_nsc_end:offset_nsc_end+0x2] = real_nameSpace_end_type
+    print("Source's nameSpaceChunk type has Fixed.")
+else:
+    print("Source's nameSpaceChunk type looks good.")
+
+# compare start & end
+if int_nameSpaceChunk_start != int_nameSpaceChunk_end:
+    print("Source's nameSpaceChunk has a problem. Fixing...")
+    barray_content[offset_nsc_end+0x4:offset_nsc_end+0x4+0x14] = int_nameSpaceChunk_start.to_bytes(0x14, 'little')
+    print("Source's nameSpaceChunk has Fixed.")
+else:
+    print("Source's nameSpaceChunk looks good.")
+    
 bytes_content = bytes(barray_content)
 
 # Making a new apk with fixed things.
